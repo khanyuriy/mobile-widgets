3f57a87d5bf14bde1b769fcb98ba062ee7b59125
Khan Yuriy <khan.yuriy@gmail.com>
2014-12-27 19:42:27 +0300

    шторка

---
+++ js/mobile-widgets.js
@@ -0,0 +1,95 @@
+﻿angular.module('mobile-widgets', [])
+/*
+side: left, top, right, bootom
+depends on hammer.js
+*/
+.directive('curtain', function () {
+    return {
+        restrict: 'C',
+        link: function ($scope, element, $attrs) {
+            var hm, panStarted, side, isHor, isVer, x = 0, y = 0,
+                LEFT = 'left', TOP = 'top', RIGHT = 'right', BOTTOM = 'bottom';
+
+            function init() {
+                hm = new Hammer(element[0]);
+                hm.get('pan').set({ enabled: true, direction: Hammer.DIRECTION_ALL });
+
+                hm.on('panstart', panStart);
+                hm.on('pan', pan);
+                hm.on('panend', panEnd);
+
+                side = $attrs.side;
+                isHor = side == LEFT || side == RIGHT;
+                isVer = side == TOP || side == BOTTOM;
+            }
+
+            function panStart(event) {
+                panStarted = true;
+                element[0].style.transition = '';
+                element[0].style.webkitTransition = '';
+                element.addClass('open');
+            }
+
+            function pan(event) {
+                if (!panStarted) return;
+
+                var size = isHor ? element[0].offsetWidth : element[0].offsetHeight,
+                    delta = isHor ? event.deltaX : event.deltaY,
+                    percent = delta / size,
+                    translateX = 0, translateY = 0;
+
+                if (isHor) {
+                    translateX = x + (percent * 100);
+                }
+                else if (isVer) {
+                    translateY = y + (percent * 100);
+                }
+
+                console.log('translateX=' + translateX + ', translateY=' + translateY);
+
+                element[0].style.webkitTransform = 'translate3d(' + translateX + '%, ' + translateY + '%, 0)';
+                element[0].style.transform = 'translate3d(' + translateX + '%, ' + translateY + '%, 0)';
+
+            }
+
+            function panEnd(event) {
+                if (!panStarted) return;
+
+                var size = isHor ? element[0].offsetWidth : element[0].offsetHeight,
+                    delta = isHor ? event.deltaX : event.deltaY,
+                    viewport = isHor ? verge.viewportW() : verge.viewportH(),
+                    extraSize = size - viewport, transformPercent;
+
+                if (Math.abs(delta) > size * 0.2) {
+                    if (delta > 0) {
+                        transformPercent = 100 - (extraSize / size * 100);
+                        if (isHor) {
+                            x = side == LEFT ? transformPercent : 0;
+                        } else {
+                            y = side == TOP ? transformPercent : 0;
+                        }
+                    } else {
+                        transformPercent = -100 + (extraSize / size * 100);
+                        if (isHor) {
+                            x = side == LEFT ? 0 : transformPercent;
+                        } else {
+                            y = side == TOP ? 0 : transformPercent;
+                        }
+                    }
+                }
+
+                if (isHor) { if (x == 0) { element.removeClass('open'); } }
+                if (isVer) { if (y == 0) { element.removeClass('open'); } }
+
+                element[0].style.transition = 'transform 0.2s ease-out';
+                element[0].style.webkitTransition = '-webkit-transform 0.2s ease-out';
+                element[0].style.webkitTransform = 'translate3d(' + x + '%,' + y + '%,0)'
+                element[0].style.transform = 'translate3d(' + x + '%,' + y + '%,0)'
+
+                panStarted = false;
+            }
+
+            init();
+        }
+    };
+})
